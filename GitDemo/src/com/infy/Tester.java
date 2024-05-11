7d7f9491101392f46300b1a0e92bbe5e63dfe76a
ychandra0501 <ychandra0501@gmail.com>
2024-05-11 14:48:59 +0530

    This is my first Commit

---
+++ GitDemo/src/com/infy/Tester.java
@@ -0,0 +1,8 @@
+package com.infy;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ GitDemo/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Chandra Mohan Yadav
+ *
+ */
+module GitDemo {
+}
